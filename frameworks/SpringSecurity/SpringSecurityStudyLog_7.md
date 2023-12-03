1fdb0527dd3803ad50f4402d6bd1d1bc66d1b5ea
hhj <hhju0720@gmail.com>
2023-12-03 23:56:12 +0900

    스프링시큐리티 RSA

--- frameworks/SpringSecurity/SpringSecurityStudyLog_7.md
+++ frameworks/SpringSecurity/SpringSecurityStudyLog_7.md
@@ -1,2 +1,142 @@
 ## JWT 인증 설정하기
-* build.gradle로 가서 OAuth2 리소스 서버를 설정하기
+* build.gradle로 가서 OAuth2 리소스 서버를 설정하기(의존성 추가)
+```` 
+implementation 'org.springframework.boot:spring-boot-starter-oauth2-resource-server'
+```` 
+* REST API를 노출시키고, 인증과 승인에 JWT를 사용할 예정
+* 기존의 'BasicAuthSecurityConfiguration' 파일의 내용을 복사해서 'JWTSecurityConfiguration'를 새로 만듦
+* 기존의 클래스는 @Configuration 주석처리,
+* JWTSecurityConfiguration은 jwt 패키지로 이동
+* 기존의 DataSource, jdbcUserDetailsManager은 그대로 사용
+
+<br>
+
+### Oauth2 리소스 서버 활성화 하기
+* shift 2번 누르고 OAuth2ResourceServerConfigurer 검색
+* OAuth2ResourceServerConfigurer클래스는 OAuth2리소스 서버 지원을 제공해줄 수 있는 클래스
+* 기본값으로 BearerTokenAuthenticationFilter를 연결해줌
+* (이전 개념에서 배운 내용) JWT는 요청 헤더의 일부로 전송될 것이며, Bearer 토큰 형태로 전송됨
+* 리소스 서버의 역할 : 이 토큰을 받아서 디코딩하고, 인증과 승인을 적절히 수행하기
+* jwt()메서드로 OAuth 서버 설정을 할 수 있음
+* 해당 메서드에서 레퍼런스 카피를 하고 현재 수정중인 JWT어쩌구 클래스의 필터체인에 붙여넣기함
+* 예시 org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer#jwt()
+````java
+
+    @Bean
+    SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.sessionManagement() .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		http.httpBasic(withDefaults()); //구문은 브라우저에 기본 인증 팝업을 표시하도록 설정
+
+        http.csrf().disable(); //csrf 사용 안함
+
+        http.headers().frameOptions().sameOrigin(); // 프레임 사용 설정
+
+        http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt); //추가한 코드
+
+
+		return http.build();
+	}
+````
+* 프로젝트 빌드 하면 실패 정보 뜸
+````
+
+***************************
+APPLICATION FAILED TO START
+***************************
+
+Description:
+
+Method SecurityFilterChain in com.udemystrudy.learnspringsecurity.jwt.JWTSecurityConfiguration
+ required a bean of type 'org.springframework.security.oauth2.jwt.JwtDecoder' 
+ that could not be found.
+````
+* (해석) SecurityFilterChain메서드가 JwtDecoder타입의 bean을 요구했는데, 그것을 찾을 수 없어서
+* 디코딩을 위해서는 JwtDecoder가 필요하다
+* 이를 위해서는 RSA 키쌍을 만들고, RSA 키 객체를 만드는 것이 선행되어야함
+
+<br>
+
+=============================================================
+
+<br>
+
+### [1] 키 쌍 만들기
+* openssl라는 툴로  RSA 공개키와 비밀키를 만들 수 있음
+* 강의에서는 코드를 통해 키 쌍을 만들 예정임(KeyPairGenerator라는 클래스를 사용해서)
+* KeyPairGenerator클래스는 java.security의 일부
+* generateKeyPair() 메서드로 키 쌍을 만들 수 있음
+````java
+//키 페어 만들기 (디코딩 사전 작업)
+@Bean
+public KeyPair keyPair(){
+        try {
+        var keyPaitrGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPaitrGenerator.initialize(2048);
+        return keyPaitrGenerator.generateKeyPair();
+//        } catch (NoSuchAlgorithmException e) {
+        } catch (Exception e) {
+        throw new RuntimeException(e);
+        }
+        }
+````
+
+<br>
+       
+### [2] RSA 객체 만들기 (키 쌍 이용해서)
+* RSA 인코딩과 디코딩을 위해 nimbus라는 라이브러리를 사용
+* nimbus는 이미 우리의 클래스 패스에 존재함(OAuth 리소스 서버 의존성에 포함)
+* nimbus를 import해야함!
+````
+//import java.security.interfaces.RSAKey;   //이거 import하면 오류남. 조심
+import com.nimbusds.jose.jwk.RSAKey;        //nimbusds import를 꼭 확인하기!(직접 작성해야함)
+````
+
+
+* RSA 객체 만드는 코드
+
+````
+    //RSA 객체 만들기
+    @Bean
+    public RSAKey rsaKey(KeyPair keyPair){
+
+        return  new RSAKey
+                .Builder((RSAPublicKey)keyPair.getPublic())
+                .privateKey(keyPair().getPrivate())
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+````
+
+<br>
+
+### [3] JSON Web Key source 만들기
+* RSA 키 하나만 있는 JWKSet 만들고 -> 이것을 이용해서 JWKSource 만들기
+* 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- frameworks/SpringSecurity/learn-spring-security/learn-spring-security/src/main/java/com/udemystrudy/learnspringsecurity/jwt/JWTSecurityConfiguration.java
+++ frameworks/SpringSecurity/learn-spring-security/learn-spring-security/src/main/java/com/udemystrudy/learnspringsecurity/jwt/JWTSecurityConfiguration.java
@@ -1,18 +1,32 @@
 package com.udemystrudy.learnspringsecurity.jwt;
 
+import com.nimbusds.jose.jwk.JWKSet;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+//import java.security.interfaces.RSAKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
+
+import com.nimbusds.jose.jwk.RSAKey; //RSA객체 만들기
+import com.nimbusds.jose.jwk.source.JWKSource;  //JWT source만들기
+
 
 import static org.springframework.security.config.Customizer.withDefaults;
 import static org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION;
@@ -25,37 +39,17 @@ public class JWTSecurityConfiguration {
 		http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
         http.sessionManagement() .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-//		http.formLogin(withDefaults());  // 폼 로그인 사용 안하기 위해 주석처리
 		http.httpBasic(withDefaults()); //구문은 브라우저에 기본 인증 팝업을 표시하도록 설정
-        http.csrf().disable();
+
+        http.csrf().disable(); //csrf 사용 안함
 
         http.headers().frameOptions().sameOrigin(); // 프레임 사용 설정
 
+        http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+
+
 		return http.build();
 	}
-
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        // 각 사용자에 대한 UserDetails 객체 생성
-//        UserDetails user1 = User.withUsername("hello")
-//                                .password("{noop}world")
-//                                .roles("USER")
-//                                .build();
-//
-//        UserDetails user2 = User.withUsername("lemon")
-//                                .password("{noop}candy")
-//                                .roles("ADMIN")
-//                                .build();
-//
-//        UserDetails user3 = User.withUsername("black")
-//                                .password("{noop}pink")
-//                                .roles("PLAYER")
-//                                .build();
-//
-//        // InMemoryUserDetailsManager에 UserDetails 객체들을 전달
-//        return new InMemoryUserDetailsManager(user1, user2, user3);
-//    }
-
 
     @Bean
     public DataSource dataSource(){
@@ -106,6 +100,44 @@ public class JWTSecurityConfiguration {
         return new BCryptPasswordEncoder();
     }
 
+
+    //키 페어 만들기 (디코딩 사전 작업)
+    @Bean
+    public KeyPair keyPair(){
+        try {
+            var keyPaitrGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPaitrGenerator.initialize(2048);
+            return keyPaitrGenerator.generateKeyPair();
+//        } catch (NoSuchAlgorithmException e) {
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    //RSA 객체 만들기
+    @Bean
+    public RSAKey rsaKey(KeyPair keyPair){
+
+        return  new RSAKey
+                .Builder((RSAPublicKey)keyPair.getPublic())
+                .privateKey(keyPair().getPrivate())
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+
+
+    //JSON Web Key source 만들기  (JWKSet 만들고 이걸로 JWKSource 만듦)
+    @Bean
+    public JWKSource jwkSource(RSAKey rsaKey){
+        var jwtSet = new JWKSet(rsaKey);
+    }
+
+
+//    @Bean
+//    public JwtDecoder jwtDecoder(){
+//        return Decoder()
+//    }
 
 
 
