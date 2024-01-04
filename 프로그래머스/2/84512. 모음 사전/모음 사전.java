8c30ea00cf6ff78f9cc651cfb6750f68046180b3
HongHyeongJu <126171435+HongHyeongJu@users.noreply.github.com>
2024-01-04 21:36:13 +0900

    [level 2] Title: 모음 사전, Time: 0.03 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -2,37 +2,24 @@ import java.util.*;
 
 class Solution {
     
-        private static final char[] VOWELS = {'A', 'E', 'I', 'O', 'U'};
-    private static int count = 0;
-    private static String target;
-    private static int answer = 0;
 
-    
     
    public int solution(String word) {
-               target = word;
+        int answer = 0;
+        int[] jump = {781, 156, 31, 6, 1}; // 각 자리가 변화될때 넘겨야할 경우의수
+                   //625+125+25+5+1    125+25+5+1   25+5+1    5+1    1
+        char[] vowels = {'A', 'E', 'I', 'O', 'U'};
 
-               DFS("");
+        for (int i = 0; i < word.length(); i++) {
+            for (int j = 0; j < 5; j++) {
+                if (word.charAt(i) == vowels[j]) {
+                    answer += 1 + j * jump[i];
+                }
+            }
+        }
 
         return answer;
-    }
-
-    public void DFS(String currentStr){
-        //현재 단어가 찾던 단어이면 count값을 answer에 대입하고 메서드 나가기
-        if(currentStr.equals(target)) {answer = count; return;}
-
-        //현재 단어의 길이가 5이면 더 붙일것도 없음 나가라
-        if(currentStr.length()==5) {return;}
-
-
-        //아직 여유가 있다면 for each 반복문으로 current에 단어 하나 더 붙이기
-        for(char a : VOWELS){
-            count++;  //횟수 추가하고
-            DFS(currentStr + a); //단어 붙여서 재귀호출
-            //이미 찾았을 때 남은 재귀호출 실행하지 않도록 하기
-            if(answer!=0) return;
             
         }
 
-    }
 }
